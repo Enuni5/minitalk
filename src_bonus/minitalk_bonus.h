7dbb30644c0086995f88b48718c25bd864eebd54
Emilio Nunez nieto <enunez-n@c2r8s3.42malaga.com>
2022-08-25 18:53:24 +0200

    bonus update

--- src_bonus/minitalk_bonus.h
+++ src_bonus/minitalk_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minitalk.h                                         :+:      :+:    :+:   */
+/*   minitalk_bonus.h                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: enunez-n <enunez-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 19:44:12 by enunez-n          #+#    #+#             */
-/*   Updated: 2022/08/22 20:23:25 by enunez-n         ###   ########.fr       */
+/*   Updated: 2022/08/25 18:53:01 by enunez-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINITALK_H
-# define MINITALK_H
+#ifndef MINITALK_BONUS_H
+# define MINITALK_BONUS_H
 
 # include "../libft/libft.h"
 # include <signal.h>
