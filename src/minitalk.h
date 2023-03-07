5516a2f5af41596bf9fe08dcac35581a11a69edb
Emilio Nunez nieto <enunez-n@student.42malaga.com>
2023-03-07 14:00:56 +0100

    solved some issues with references of libft

--- src/minitalk.h
+++ src/minitalk.h
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   minitalk.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: enunez-n <enunez-n@student.42.fr>          +#+  +:+       +#+        */
+/*   By: enunez-n <enunez-n@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 19:44:12 by enunez-n          #+#    #+#             */
-/*   Updated: 2022/08/22 20:23:25 by enunez-n         ###   ########.fr       */
+/*   Updated: 2023/03/07 14:00:19 by enunez-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
 
-# include "../libft/libft.h"
+# include "../../libft/libft.h"
 # include <signal.h>
 # include <stdlib.h>
 # include <sys/wait.h>
--- src_bonus/minitalk_bonus.h
+++ src_bonus/minitalk_bonus.h
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   minitalk_bonus.h                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: enunez-n <enunez-n@student.42.fr>          +#+  +:+       +#+        */
+/*   By: enunez-n <enunez-n@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 19:44:12 by enunez-n          #+#    #+#             */
-/*   Updated: 2022/08/25 18:53:01 by enunez-n         ###   ########.fr       */
+/*   Updated: 2023/03/07 14:00:24 by enunez-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_BONUS_H
 # define MINITALK_BONUS_H
 
-# include "../libft/libft.h"
+# include "../../libft/libft.h"
 # include <signal.h>
 # include <stdlib.h>
 # include <sys/wait.h>
