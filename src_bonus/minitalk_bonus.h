03ee05667bcacef28592d34ad8a0aceb5b75cbc1
Emilio Nunez nieto <enunez-n@student.42malaga.com>
2023-03-09 16:30:52 +0100

    Fix references of libft in the project

--- src_bonus/minitalk_bonus.h
+++ src_bonus/minitalk_bonus.h
@@ -6,14 +6,14 @@
 /*   By: enunez-n <enunez-n@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/22 19:44:12 by enunez-n          #+#    #+#             */
-/*   Updated: 2023/03/07 14:00:24 by enunez-n         ###   ########.fr       */
+/*   Updated: 2023/03/09 16:29:12 by enunez-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_BONUS_H
 # define MINITALK_BONUS_H
 
-# include "../../libft/libft.h"
+# include "../libft/libft.h"
 # include <signal.h>
 # include <stdlib.h>
 # include <sys/wait.h>
